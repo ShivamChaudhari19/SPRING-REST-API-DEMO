3fb5ea8f9b4a11de9e22aaf2281f056c962c573d
shivam_chaudhari19 <shivamsc7289@gmail.com>
2024-10-07 15:34:28 +0530

    comment added

--- demo/src/main/java/com/demo/demo/DemoApplication.java
+++ demo/src/main/java/com/demo/demo/DemoApplication.java
@@ -3,13 +3,14 @@ package com.demo.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//REST API
 @SpringBootApplication
 @AutoConfiguration
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
 	}
 
 }
